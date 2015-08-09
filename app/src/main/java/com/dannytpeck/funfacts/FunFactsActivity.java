00ce67d83112ad1d8313970acd10061299b351e4
Danny T. Peck <dannytpeck@gmail.com>
2015-08-08 22:29:04 -0700

    Move facts to their own class

--- app/src/main/java/com/dannytpeck/funfacts/FunFactsActivity.java
+++ app/src/main/java/com/dannytpeck/funfacts/FunFactsActivity.java
@@ -1,17 +1,17 @@
 package com.dannytpeck.funfacts;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Button;
 
-import java.util.Random;
-
 
 public class FunFactsActivity extends Activity {
+
+    private FactBook mFactBook = new FactBook();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,60 +21,19 @@ public class FunFactsActivity extends Activity {
         // Declare our View variables and assign them the Views from the layout file
         final TextView factLabel = (TextView) findViewById(R.id.factTextView);
         Button showFactButton = (Button) findViewById(R.id.showFactButton);
+        final RelativeLayout relativeLayout = (RelativeLayout) findViewById(R.id.relativeLayout);
+
         View.OnClickListener listener = new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String[] facts = {
-                        "Ants stretch when they wake up in the morning.",
-                        "Ostriches can run faster than horses.",
-                        "Olympic gold medals are actually made mostly of silver.",
-                        "You are born with 300 bones; by the time you are an adult you will have 206.",
-                        "It takes about 8 minutes for light from the Sun to reach Earth.",
-                        "Some bamboo plants can grow almost a meter in just one day.",
-                        "The state of Florida is bigger than England.",
-                        "Some penguins can leap 2-3 meters out of the water.",
-                        "On average, it takes 66 days to form a new habit.",
-                        "Mammoths still walked the earth when the Great Pyramid was being built." };
-
-                // The button was clicked, so update the fact label with a new fact
-                String fact = "";
-
-                // Randomly select a fact
-                Random randomGenerator = new Random(); // Construct a new Random number generator
-                int randomNumber = randomGenerator.nextInt(10);
-
-                /* Convert the randomNumber to a text fact
-                 * 0 = Ants stretch when they wake up in the morning.
-                 * 1 = Ostriches can run faster than horses.
-                 * 2 = Olympic gold medals are actually made mostly of silver.
-                 */
-
-                fact = facts[randomNumber];
-
+                String fact = mFactBook.getFact();
                 // Update the label with our dynamic fact
                 factLabel.setText(fact);
+                relativeLayout.setBackgroundColor(Color.RED);
             }
         };
         showFactButton.setOnClickListener(listener);
-    }
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.fun_facts, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
     }
 }
